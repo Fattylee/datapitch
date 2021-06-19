ebd08054fefd0713c29a7fe9eed1647d29b192ca
fatai balogun <fattylee.remod@gmail.com>
2021-06-19 23:30:57 +0100

    complete footer

--- src/pages/home/Footer.js
+++ src/pages/home/Footer.js
@@ -15,6 +15,7 @@ export const Footer = () => {
   return (
     <Box
       as="footer"
+      id="contact"
       bgGradient="linear(124.34deg, #70317E 0%, #282938 41.28%)"
       pt="56px"
     >
@@ -26,11 +27,15 @@ export const Footer = () => {
           color="white"
           mb="54px"
         >
-          <Box w="100%">
+          <Box w="100%" textAlign={{ base: 'center', md: 'initial' }}>
             <Text as="h4" fontSize="48px" lineHeight="62.21px" mb="16px">
               Join Community
             </Text>
-            <Text lineHeight="21.86px" maxWidth="368px">
+            <Text
+              lineHeight="21.86px"
+              maxWidth="368px"
+              mx={{ base: 'auto', md: 'initial' }}
+            >
               Follow us on our social media platforms for our latest updates and
               promotions
             </Text>
@@ -39,7 +44,7 @@ export const Footer = () => {
             as="ul"
             justifyContent={{ base: 'space-around', md: 'flex-end' }}
             w="100%"
-            mt={{ base: '40px', md: 'auto' }}
+            mt={{ base: '40px', md: '0' }}
           >
             {socialLinks.map(({ href, icon: Icon }, index) => (
               <Link key={index} href={href} ml={{ md: '40px' }}>
@@ -49,14 +54,96 @@ export const Footer = () => {
           </Flex>
         </Flex>
         <Box
-          px={{ base: '16px', md: '114px' }}
+          px={{ base: '16px', md: '80px', lg: '114px' }}
           pt="63px"
           pb="37px"
           rounded="100px 100px 0px 0px"
           bgGradient="linear-gradient(122.42deg, #0C1018 0%, #0C1018 46.96%, #2D2F36 82.08%)"
         >
-          <Box borderTop="1px solid #f2f2f2" mb="21px"></Box>
-          <HStack spacing="45px">
+          <Flex
+            // fontFamily="Avenir" not available for free
+            fontSize="14px"
+            lineHeight="19.12px"
+            listStyleType="none"
+            flexDir={{ base: 'column', md: 'row' }}
+          >
+            <Box
+              fontSize="48px"
+              lineHeight="65.57px"
+              fontWeight="extrabold"
+              mr="34px"
+              mb="24px"
+              textAlign={{ base: 'center', md: 'initial' }}
+            >
+              <Link href="/#">Datapitch</Link>
+            </Box>
+            <Flex
+              flexDir={{ base: 'row', md: 'column' }}
+              justify={{ base: 'space-between' }}
+              mr="40px"
+              w="100%"
+            >
+              <Box as="li" mb="24px">
+                <Link href="#request-a-demo">REQUEST A DEMO</Link>
+              </Box>
+              <Box as="li" mb="24px">
+                <Link
+                  href="#request-a-demo"
+                  mb="24px"
+                  ml={{ base: '-13px', md: '0px' }}
+                >
+                  LEARN MORE
+                </Link>
+              </Box>
+              <Box as="li" mb="33px">
+                <Link href="#request-a-demo" mb="24px">
+                  JOIN OUR C
+                </Link>
+              </Box>
+            </Flex>
+
+            <Flex
+              width="100%"
+              justify={{ base: 'space-between', md: 'normal' }}
+              flexDir={{ md: 'column' }}
+              mr="40px"
+            >
+              <Box as="li" mb="24px">
+                <Link href="#request-a-demo">GET HELP</Link>
+              </Box>
+              <Box as="li" mb="24px">
+                <Link href="#request-a-demo" mb="24px">
+                  FAQS
+                </Link>
+              </Box>
+              <Box as="li" mb="33px">
+                <Link href="#request-a-demo" mb="24px">
+                  CONTACT US
+                </Link>
+              </Box>
+            </Flex>
+            <Flex
+              flexDir={{ md: 'column' }}
+              justify={{ base: 'space-between', md: 'normal' }}
+              listStyleType="none"
+              bg="maaroon"
+              w="100%"
+            >
+              <Box as="li" mb="24px">
+                <Link href="#request-a-demo">WHAT WE DO</Link>
+              </Box>
+              <Box as="li" mb="24px">
+                <Link href="#request-a-demo" mb="24px">
+                  WHO WE ARE
+                </Link>
+              </Box>
+            </Flex>
+          </Flex>
+          <Box borderTop="1px solid #f2f2f2" mb="21px" opacity="0.2"></Box>
+          <HStack
+            spacing="45px"
+            justify={{ base: 'space-around', md: 'normal' }}
+          >
             <Link href="#privacy-policy">PRIVACY POLICY</Link>
             <Link href="#terms-and-condition">TERMS & CONDITIONS</Link>
           </HStack>
